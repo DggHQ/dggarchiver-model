4e7eda077864bf05e328caa29a74b4a89dc390f6
Kevin Christen <admin@0x01.host>
2022-09-25 11:16:43 +0200

    add initial structures to be able to reuse them accross multple parts of the service

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DggHQ/dggarchiver-model
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+package dggarchivermodel
+
+// Contains the data structure for the LiveNotify message
+type LiveNotify struct {
+	Live bool
+}
+
+// Contains the data structure for the LiveNotifyReply message
+type LiveNotifyReply struct {
+	Mute bool
+}
