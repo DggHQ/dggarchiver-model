03ef5d760d1e2be0c2f53a7a52422b7e1e845ecf
vyneer <vyn33r@gmail.com>
2024-07-18 13:30:25 +0300

    fix: make tags field work

--- dggarchivermodel.go
+++ dggarchivermodel.go
@@ -1,10 +1,34 @@
 package dggarchivermodel
 
 import (
+	"database/sql/driver"
 	"encoding/json"
+	"fmt"
+	"strings"
 
 	"gorm.io/gorm"
 )
+
+type Tags []string
+
+func (t *Tags) Scan(src interface{}) error {
+	bytes, ok := src.([]byte)
+	if !ok {
+		return fmt.Errorf("Failed to unmarshal []string value: %+v", src)
+	}
+
+	*t = strings.Split(string(bytes), ",")
+
+	return nil
+}
+
+func (t Tags) Value() (driver.Value, error) {
+	if len(t) == 0 {
+		return nil, nil
+	}
+
+	return strings.Join(t, ","), nil
+}
 
 // Contains the data structure for the LiveNotify message
 type LiveNotify struct {
@@ -18,21 +42,21 @@ type LiveNotifyReply struct {
 
 // Contains the data structure for any VOD/livestream
 type VOD struct {
-	Platform      string   `json:"platform" gorm:"index:idx_vid_platform_hostplatform,unique"`
-	Downloader    string   `json:"downloader"`
-	VID           string   `json:"id" gorm:"index:idx_vid_platform_hostplatform,unique"`
-	PlaybackURL   string   `json:"playbackurl"`
-	PubTime       string   `json:"pubtime"`
-	Title         string   `json:"title"`
-	StartTime     string   `json:"starttime"`
-	EndTime       string   `json:"endtime"`
-	Thumbnail     string   `json:"thumbnail"`
-	ThumbnailPath string   `json:"thumbnailpath"`
-	Path          string   `json:"path"`
-	Duration      int      `json:"duration"`
-	Visibility    int      `json:"visibility"` // 0 is public, 1 is unlisted, 2 is private
-	Quality       string   `json:"quality"`
-	Tags          []string `json:"tags"`
+	Platform      string `json:"platform" gorm:"index:idx_vid_platform_hostplatform,unique"`
+	Downloader    string `json:"downloader"`
+	VID           string `json:"id" gorm:"index:idx_vid_platform_hostplatform,unique"`
+	PlaybackURL   string `json:"playbackurl"`
+	PubTime       string `json:"pubtime"`
+	Title         string `json:"title"`
+	StartTime     string `json:"starttime"`
+	EndTime       string `json:"endtime"`
+	Thumbnail     string `json:"thumbnail"`
+	ThumbnailPath string `json:"thumbnailpath"`
+	Path          string `json:"path"`
+	Duration      int    `json:"duration"`
+	Visibility    int    `json:"visibility"` // 0 is public, 1 is unlisted, 2 is private
+	Quality       string `json:"quality"`
+	Tags          Tags   `json:"tags" gorm:"type:string"`
 }
 
 // Contains the data structure to add the VOD into the SQLite DB
